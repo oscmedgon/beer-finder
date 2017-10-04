338f9c755599809f98deabd25593602bccae7163
waken22 <waken22jrb@gmail.com>
2017-10-04 16:05:23 +0200

    Fixed some bugs about the default values and added new theme

--- scripts/script.js
+++ scripts/script.js
@@ -40,16 +40,22 @@
       })
       .then(function(oData) {
         console.log(oData)
-        var defaultDesc = "This beer should be bad as hell... because they didn't have a description"
+        // Default Variables in the case they don't have the value in the prop
+        var defaultName = "Error 404, name not found, lol"
         var defaultImg = "https://tapintothenuttyirishman.files.wordpress.com/2016/02/beer-glass-question-mark.jpg?w=584"
+        var defaultDesc = "This beer should be bad as hell... because they didn't have a description"
         var defaultDate = "This beer it's older than Jesus"
-        var beerName = oData.name
+        var defaultAbv = "No average... why not?"
+        var defaultStyle = "No style... because yolooo!"
+        var defaultServing = "The temperature will be more than 9000"
+        
+        var beerName = oData.name && true && oData.name || defaultName
         var beerImg = oData.labels && oData.labels.medium && oData.labels.medium || defaultImg
         var beerDesc = oData.description && true && oData.description || defaultDesc
         var beerCreationDate = oData.glass && oData.glass.createDate && oData.glass.createDate || defaultDate
-        var beerAbv = oData.abv
-        var beerStyle = oData.style.category.name
-        var beerServing = oData.servingTemperatureDisplay
+        var beerAbv = oData.abv && oData.abv || defaultAbv
+        var beerStyle = oData.style && oData.style.category && oData.style.category.name && oData.style.category.name || defaultStyle
+        var beerServing = oData.servingTemperatureDisplay && oData.servingTemperatureDisplay || defaultServing
 
 
         $('#beerName').text(beerName)
